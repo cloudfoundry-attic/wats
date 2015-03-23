e850c381882eb1dc1bd1ff785a0fa261b3de6605
Dave goddard & Ben Moss <pair+dgoddard+bmoss@pivotal.io>
2015-03-23 10:55:41 -0400

    Use msbuild instead of devenv [#90905976]

--- make.bat
+++ make.bat
@@ -5,5 +5,5 @@ if errorLevel 1 ( echo "devenv was not found on PATH" && exit /b 1 )
  
 rmdir /S /Q packages
 bin\nuget restore || exit /b 1
-devenv Nora.sln /build "Release" || exit /b 1
+MSBuild Nora.sln /t:Rebuild /p:Configuration=Release || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Nora.Tests\bin\Release\Nora.Tests.dll || exit /b 1
