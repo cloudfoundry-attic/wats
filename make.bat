dd86df712713c0fe1808b0f62c230f70978335be
Dave goddard & Ben Moss <pair+dgoddard+bmoss@pivotal.io>
2015-03-23 10:16:48 -0400

    Create make script [#90818444]

---
+++ make.bat
@@ -0,0 +1,9 @@
+:: Visual Studio must be in path
+
+where devenv
+if errorLevel 1 ( echo "devenv was not found on PATH" && exit /b 1 )
+ 
+rmdir /S /Q packages
+bin\nuget restore || exit /b 1
+devenv Nora.sln /build "Release" || exit /b 1
+packages\nspec.0.9.68\tools\NSpecRunner.exe Nora.Tests\bin\Release\Nora.Tests.dll || exit /b 1
