052a5f61c709aa85895a5ede7dc619639dfad5d5
Ben Moss and John Shahid <pair+bmoss+jvshahid@pivotal.io>
2015-04-08 11:54:19 -0400

    Add a script for running on ci

--- scripts/run_wats.sh
+++ scripts/run_wats.sh
@@ -9,4 +9,17 @@ go get github.com/onsi/ginkgo/ginkgo
 # The following line will fail with the || echo, since tests don't
 # have a binary and go get will try to build one
 go get -t ../tests/... 2>/dev/null || echo "Installed dependencies"
-ginkgo -r -failFast -slowSpecThreshold=120 $@ ../tests
+
+tempfile=`mktemp -t wats`
+trap "rm -f $tempfile" EXIT
+cat > $tempfile <<HERE
+{
+  "api": "$API",
+  "admin_user": "$ADMIN_USER",
+  "admin_password": "$ADMIN_PASSWORD",
+  "apps_domain": "$APPS_DOMAIN",
+  "secure_address": "$SECURE_ADDRESS",
+  "skip_ssl_validation": true
+}
+HERE
+CONFIG=$tempfile ginkgo -r -failFast -slowSpecThreshold=120 $@ ../tests
