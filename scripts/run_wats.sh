f20b0bfd3f28eae2818f4c9e417cf88d2aef19f1
Ben_Moss <bmoss@pivotal.io>
2015-06-11 11:54:12 -0400

    Fix run_wats.sh
    - Specify bash
    - Add set -x to fail on errors
    - Don't accept empty environment variables
    
    Signed-off-by: John_Shahid <jshahid@pivotal.io>

--- scripts/run_wats.sh
+++ scripts/run_wats.sh
@@ -1,10 +1,13 @@
-set -e
-: ${API?"Must set api url (e.g. api.10.244.0.34.xip.io)"}
-: ${ADMIN_USER?"Must set admin username (e.g. admin)"}
-: ${ADMIN_PASSWORD?"Must set admin password (e.g. admin)"}
-: ${APPS_DOMAIN?"Must set app domain url (e.g. 10.244.0.34.xip.io)"}
-: ${SOCKET_ADDRESS_FOR_SECURITY_GROUP_TEST?"Must set address [ip address of Diego ETCD cluster] (e.g. 10.244.16.2:4001)"}
-: ${DOPPLER_URL?"Must set doppler websocket url (e.g. wss://doppler.hello.cf-app.com:4443)"}
+#!/usr/bin/env bash
+
+set -ex
+
+: ${API:?"Must set api url (e.g. api.10.244.0.34.xip.io)"}
+: ${ADMIN_USER:?"Must set admin username (e.g. admin)"}
+: ${ADMIN_PASSWORD:?"Must set admin password (e.g. admin)"}
+: ${APPS_DOMAIN:?"Must set app domain url (e.g. 10.244.0.34.xip.io)"}
+: ${SOCKET_ADDRESS_FOR_SECURITY_GROUP_TEST:?"Must set address [ip address of Diego ETCD cluster] (e.g. 10.244.16.2:4001)"}
+: ${DOPPLER_URL:?"Must set doppler websocket url (e.g. wss://doppler.hello.cf-app.com:4443)"}
 
 cd `dirname $0`
 
