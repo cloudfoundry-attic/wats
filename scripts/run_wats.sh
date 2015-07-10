890cf12a780270e5beb338becc70c0cf70dbb6c1
Ben_Moss <bmoss@pivotal.io>
2015-07-10 11:56:38 -0400

    Support passing a config file as the argument to script/run_wats
    Signed-off-by: Stefan_Schneider <stefan.schneider@hp.com>

--- scripts/run_wats.sh
+++ scripts/run_wats.sh
@@ -4,12 +4,30 @@ set -ex
 
 cd `dirname $0`
 
+if [ -f "$1" ]; then
+  CONFIG_FILE=$PWD/$1
+else
+  CONFIG_FILE=`mktemp -t watsXXXXX`
+  trap "rm -f $CONFIG_FILE" EXIT
+
 : ${API:?"Must set api url (e.g. api.10.244.0.34.xip.io)"}
 : ${ADMIN_USER:?"Must set admin username (e.g. admin)"}
 : ${ADMIN_PASSWORD:?"Must set admin password (e.g. admin)"}
 : ${APPS_DOMAIN:?"Must set app domain url (e.g. 10.244.0.34.xip.io)"}
 : ${SOCKET_ADDRESS_FOR_SECURITY_GROUP_TEST:?"Must set address [ip address of Diego ETCD cluster] (e.g. 10.244.16.2:4001)"}
 : ${DOPPLER_URL:?"Must set doppler websocket url (e.g. wss://doppler.hello.cf-app.com:4443)"}
+
+cat > $CONFIG_FILE <<HERE
+{
+  "api": "$API",
+  "admin_user": "$ADMIN_USER",
+  "admin_password": "$ADMIN_PASSWORD",
+  "apps_domain": "$APPS_DOMAIN",
+  "secure_address": "$SOCKET_ADDRESS_FOR_SECURITY_GROUP_TEST",
+  "skip_ssl_validation": true
+}
+HERE
+fi
 
 if [[ "$(uname)" = "Darwin" ]]; then
   ln -sf cf-darwin ../bin/cf
@@ -28,16 +46,4 @@ go install github.com/onsi/ginkgo/ginkgo
 # have a binary and go get will try to build one
 go install -t ../tests/... 2>/dev/null || echo "Installed dependencies"
 
-tempfile=`mktemp -t watsXXXXX`
-trap "rm -f $tempfile" EXIT
-cat > $tempfile <<HERE
-{
-  "api": "$API",
-  "admin_user": "$ADMIN_USER",
-  "admin_password": "$ADMIN_PASSWORD",
-  "apps_domain": "$APPS_DOMAIN",
-  "secure_address": "$SOCKET_ADDRESS_FOR_SECURITY_GROUP_TEST",
-  "skip_ssl_validation": true
-}
-HERE
-CONFIG=$PWD/"bosh_lite_config.json" ginkgo -r -v -failFast -slowSpecThreshold=120 $@ ../wats
+CONFIG=$CONFIG_FILE ginkgo -r -v -failFast -slowSpecThreshold=120 $@ ../wats
