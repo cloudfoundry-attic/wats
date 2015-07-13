b53e30c5e85c6d8141e0c660e7ba1ad1343e39ff
Dave Goddard <dgoddard@pivotal.io>
2015-07-13 09:17:02 -0400

    Add Forkbomb WATs test
    Wats now require ENV[NUM_WIN_CELLS] so that it can test that the
    machines are all available both before and after a fork bomb.
    
    Signed-off-by: Stefan_Schneider <stefan.schneider@hp.com>

--- scripts/run_wats.sh
+++ scripts/run_wats.sh
@@ -14,6 +14,7 @@ else
 : ${APPS_DOMAIN:?"Must set app domain url (e.g. 10.244.0.34.xip.io)"}
 : ${SOCKET_ADDRESS_FOR_SECURITY_GROUP_TEST:?"Must set address [ip address of Diego ETCD cluster] (e.g. 10.244.16.2:4001)"}
 : ${DOPPLER_URL:?"Must set doppler websocket url (e.g. wss://doppler.hello.cf-app.com:4443)"}
+: ${NUM_WIN_CELLS:?"Must provide the number of windows cells in this deploy (e.g. 2)"}
 
 cat > $CONFIG_FILE <<HERE
 {
@@ -45,4 +46,4 @@ go install github.com/onsi/ginkgo/ginkgo
 # have a binary and go get will try to build one
 go install -t ../tests/... 2>/dev/null || echo "Installed dependencies"
 
-CONFIG=$CONFIG_FILE ginkgo -r -v -slowSpecThreshold=120 $@ ../wats
+NUM_WIN_CELLS=$NUM_WIN_CELLS CONFIG=$CONFIG_FILE ginkgo -r -v -slowSpecThreshold=120 $@ ../wats
---
+++ wats/fork_bomb_test.go
@@ -0,0 +1,80 @@
+package wats
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
+)
+
+var _ = Describe("Application Lifecycle", func() {
+	var appName string
+
+	BeforeEach(func() {
+		appName = generator.RandomName()
+	})
+
+	AfterEach(func() {
+		Eventually(cf.Cf("logs", appName, "--recent")).Should(Exit())
+		Eventually(cf.Cf("delete", appName, "-f")).Should(Exit(0))
+	})
+
+	reportedComputerNames := func(instances int) map[string]bool {
+		timer := time.NewTimer(time.Second * 120)
+		defer timer.Stop()
+		run := true
+		go func() {
+			<-timer.C
+			run = false
+		}()
+
+		seenComputerNames := map[string]bool{}
+		for len(seenComputerNames) != instances && run == true {
+			seenComputerNames[helpers.CurlApp(appName, "/ENV/COMPUTER_NAME")] = true
+		}
+
+		return seenComputerNames
+	}
+
+	Describe("An app staged on Diego and running on Diego", func() {
+		It("attempts to forkbomb the environment", func() {
+			numWinCells, err := strconv.Atoi(os.Getenv("NUM_WIN_CELLS"))
+			Expect(err).NotTo(HaveOccurred())
+
+			By("pushing it", func() {
+				Eventually(pushNora(appName), CF_PUSH_TIMEOUT).Should(Succeed())
+			})
+
+			By("staging and running it on Diego", func() {
+				enableDiego(appName)
+				disableSsh(appName)
+				Eventually(runCf("scale", appName, "-i", strconv.Itoa(numWinCells*3), "-m", "512M")).Should(Succeed())
+				Eventually(runCf("start", appName), CF_PUSH_TIMEOUT).Should(Succeed())
+			})
+
+			By("verifying it's up", func() {
+				Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("hello i am nora"))
+			})
+
+			By("storing the current computer names")
+			computerNames := reportedComputerNames(numWinCells)
+
+			By("Running fork bomb", func() {
+				helpers.CurlApp(appName, "/breakoutbomb")
+				time.Sleep(3 * time.Second)
+			})
+
+			By("Making sure the bomb did not take down the machine", func() {
+				newComputerNames := reportedComputerNames(numWinCells)
+				Expect(newComputerNames).To(Equal(computerNames))
+			})
+		})
+	})
+})
