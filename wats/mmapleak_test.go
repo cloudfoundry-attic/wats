9bc7335b61d4048c6b23325b0aafc9d914cb00ec
David_Morhovich <dmorhovich@pivotal.io>
2015-07-14 11:41:53 -0400

    Adds memleak WATS test.
    [#97634084]
    
    Signed-off-by: Stefan_Schneider <stefan.schneider@hp.com>

---
+++ wats/mmapleak_test.go
@@ -0,0 +1,59 @@
+package wats
+
+import (
+	"fmt"
+	"strconv"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
+)
+
+var _ = Describe("Application Lifecycle", func() {
+	var appName string
+
+	BeforeEach(func() {
+		appName = generator.RandomName()
+	})
+
+	AfterEach(func() {
+		Eventually(cf.Cf("logs", appName, "--recent")).Should(Exit())
+		Eventually(cf.Cf("delete", appName, "-f")).Should(Exit(0))
+	})
+
+	Describe("An app staged on Diego and running on Diego", func() {
+		XIt("attempts to leak mmap", func() {
+			By("pushing it", func() {
+				Eventually(pushNora(appName), CF_PUSH_TIMEOUT).Should(Succeed())
+				enableDiego(appName)
+				disableSsh(appName)
+				Eventually(runCf("start", appName), CF_PUSH_TIMEOUT).Should(Succeed())
+			})
+
+			By("verifying it's up", func() {
+				Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("hello i am nora"))
+			})
+
+			By("requesting current memory commit charge")
+			commitCharge := helpers.CurlApp(appName, "/commitcharge")
+			commitChargeValue, err := strconv.ParseInt(commitCharge, 10, 64)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Running mmapleak (3Gb)", func() {
+				helpers.CurlApp(appName, fmt.Sprintf("/mmapleak/{%#v}", int64(3)*1024*1024*1024))
+			})
+
+			By("Commit Charge should not have changed by more than container max (2Gb)", func() {
+				newCommitCharge := helpers.CurlApp(appName, "/commitcharge")
+				newCommitChargeValue, err := strconv.ParseInt(newCommitCharge, 10, 64)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(newCommitChargeValue - commitChargeValue).To(BeNumerically("<", (int64(2) * 1024 * 1024 * 1024)))
+			})
+		})
+	})
+})
