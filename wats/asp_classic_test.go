af669382b4639e7605afc23f1dc8d48d8bfa5dd1
Ben_Moss <bmoss@pivotal.io>
2015-08-27 09:48:55 -0400

    Add a test for asp classic applications
    [#101255726]
    
    Signed-off-by: John_Shahid <jshahid@pivotal.io>

---
+++ wats/asp_classic_test.go
@@ -0,0 +1,21 @@
+package wats
+
+import (
+	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ASP classic applications", func() {
+	It("exercises the app through its lifecycle", func() {
+		By("pushing it")
+		Eventually(pushApp(appName, "../assets/asp-classic", 1, "2g")).Should(Succeed())
+
+		By("staging and running it on Diego")
+		enableDiego(appName)
+		Eventually(runCf("start", appName), CF_PUSH_TIMEOUT).Should(Succeed())
+
+		By("verifying it's up")
+		Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("Hello World!"))
+	})
+})
