515aa8ab2a79c622b7c566e6c9c3399543f4485a
Pivotal <cf-netgarden-eng@pivotal.io>
2016-03-24 16:58:38 -0400

    Revert "Vendor wats dependencies"
    This reverts commit cbba95f46c5117e8649b482d224d0cbc86c052bd.

--- scripts/run_wats.sh
+++ scripts/run_wats.sh
@@ -28,19 +28,39 @@ cat > $CONFIG_FILE <<HERE
 HERE
 fi
 
-pushd `dirname $0`
+cd `dirname $0`
 if [[ "$(uname)" = "Darwin" ]]; then
   ln -sf cf-darwin ../bin/cf
 else
   ln -sf cf-linux ../bin/cf
 fi
-popd
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-export PATH=$DIR/bin:$PATH
-export GO15VENDOREXPERIMENT=1
+export PATH=$PWD/../bin:$PATH
 
-go install github.com/onsi/ginkgo/ginkgo
+uname_s=$(uname -s | cut -d_ -f1)
+win_uname="MINGW32"
+if [ "x${uname_s}" == "x${win_uname}" ]; then
+    gopath=/tmp/watsgopath
+    rm -rf $gopath
+    ginkgo_args="-noColor"
+else
+    gopath=$(mktemp -d /tmp/watsXXXX)
+fi
+
+function cleanup {
+    rm -rf $gopath
+}
+trap cleanup EXIT
+
+export GOPATH=$gopath
+export GOBIN=$GOPATH/bin
+export PATH=$GOBIN:$PATH
+echo $GOPATH $GOBIN $PATH
+
+go get github.com/onsi/ginkgo/ginkgo
+mkdir -p $GOPATH/src/github.com/cloudfoundry-incubator/wats
+cp -R ../ $GOPATH/src/github.com/cloudfoundry-incubator/wats
+go get -t github.com/cloudfoundry-incubator/wats/...
 
 shift || true
 NUM_WIN_CELLS=$NUM_WIN_CELLS CONFIG=$CONFIG_FILE ginkgo ${ginkgo_args} -r -slowSpecThreshold=120 $@ ../wats
