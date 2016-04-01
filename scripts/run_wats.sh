a8f81155cfb7eb3e79dc2a85f3cf76d3dab02276
Matthew Horan <mhoran@pivotal.io>
2016-04-01 10:35:06 -0400

    Install ginkgo properly

--- scripts/run_wats.sh
+++ scripts/run_wats.sh
@@ -55,7 +55,7 @@ export GOBIN=$GOPATH/bin
 export PATH=$GOBIN:$PATH
 export GO15VENDOREXPERIMENT=1
 
-go install wats/vendor/github.com/onsi/ginkgo
+go install wats/vendor/github.com/onsi/ginkgo/ginkgo
 
 shift || true
 NUM_WIN_CELLS=$NUM_WIN_CELLS CONFIG=$CONFIG_FILE ginkgo ${ginkgo_args} -r -slowSpecThreshold=120 $@ ../wats
