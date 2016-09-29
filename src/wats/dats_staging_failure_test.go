4c49309f00b3fb74d2f65cf2413f1fea4bb59d93
Amin Jamali <aminjam.software@gmail.com>
2016-09-29 19:43:59 -0400

    CC has stopped emitting the error to the log
    as of CF release release-candidate
    (https://github.com/cloudfoundry/cf-release/commit/7dbb775af116d1dbaed41432f79940d3ca6278fa) CC upgrade has stopped emitting the insufficient resources log meesage

--- src/wats/dats_staging_failure_test.go
+++ src/wats/dats_staging_failure_test.go
@@ -28,12 +28,9 @@ var _ = Describe("When staging fails", func() {
 		})
 
 		It("informs the user in the CLI output and the logs", func() {
-			logs := cf.Cf("logs", appName)
 
 			start := cf.Cf("start", appName)
 			Eventually(start, CF_PUSH_TIMEOUT).Should(Exit(1))
-
-			Eventually(logs.Out).Should(gbytes.Say("Failed to stage application: insufficient resources"))
 
 			app := cf.Cf("app", appName)
 			Eventually(app).Should(Exit(0))
