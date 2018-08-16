5352272824a75ab341ffaff116d0536edb41cd63
Amin Jamali <ajamali@pivotal.io>
2018-08-16 17:34:28 -0400

    fix path for published nora

--- scripts/make.bat
+++ scripts/make.bat
@@ -1,4 +1,5 @@
 @echo on
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+nuget restore
 msbuild .\nora.sln /t:Rebuild /p:Configuration=Release /p:PublishProfile=FolderProfile /p:DeployOnBuild=true
