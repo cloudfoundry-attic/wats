fd8a7aff7de8548a7c3a6998c72245d329698272
Arjun Sreedharan <asreedharan@pivotal.io>
2018-08-22 16:46:37 -0400

    WIP
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- scripts/make.bat
+++ scripts/make.bat
@@ -1,5 +1,8 @@
 @echo on
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+
+WHERE mycommand
+IF %ERRORLEVEL% NEQ 0 ECHO nuget should be installed from (https://dist.nuget.org/win-x86-commandline/latest/nuget.exe)
 nuget restore
 msbuild .\nora.sln /t:Rebuild /p:Configuration=Release /p:PublishProfile=FolderProfile /p:DeployOnBuild=true
